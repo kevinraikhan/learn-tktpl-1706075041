1b1d0b8ae788d48103d477840f5fb1506e5f4ff3
Kevin Raikhan Zain <kevinraikhan@gmail.com>
2020-12-08 23:45:02 +0700

    Added lab-7 native

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/kevinraikhanzain/mobileprogramminglab/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kevinraikhanzain/mobileprogramminglab/MainActivity.kt
@@ -1,13 +1,16 @@
 package id.ac.ui.cs.mobileprogramming.kevinraikhanzain.mobileprogramminglab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textViewBottom.text = "Coba ubah lewat MainActivity.kt"
+
+        buttonHello.setOnClickListener {
+            textViewTop.text = "Hello, ${editTextTextPersonName.text.toString()}"
+        }
     }
 }
